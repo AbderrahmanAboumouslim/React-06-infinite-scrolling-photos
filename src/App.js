0fba425918a2c2cd012c2a9f1ad5f3edf9ac689a
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2021-11-06 23:46:56 +0100

    serverless fun

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
 import api_key from "./apikey";
-// ${process.env.REACT_APP_API_KEY}
 const API_KEY = `?client_id=${api_key}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
