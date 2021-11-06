a105705d2caee6a3b07612bcf085260f5aaf9e8e
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2021-11-06 17:31:27 +0100

    delete this shit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,136 @@
-import React, { useState, useEffect } from "react";
-import { FaSearch } from "react-icons/fa";
-import Photo from "./Photo";
+// import React, { useState, useEffect } from "react";
+// import { FaSearch } from "react-icons/fa";
+// import Photo from "./Photo";
 
-const API_KEY = `?client_id=${process.env.REACT_APP_API_KEY}`;
+// const API_KEY = `?client_id=${process.env.REACT_APP_API_KEY}`;
+// const mainUrl = `https://api.unsplash.com/photos/`;
+// const searchUrl = `https://api.unsplash.com/search/photos/`;
+
+// function App() {
+//   const [loading, setLoading] = useState(false);
+//   const [photos, setPhotos] = useState([]);
+//   const [showPage, setShowPage] = useState(1);
+//   const [query, setQuery] = useState("");
+
+//   const fetchPhotos = async () => {
+//     setLoading(true);
+//     let url;
+
+//     const urlPage = `&page=${showPage}`;
+//     const urlQuery = `&query=${query}`;
+
+//     if (query) {
+//       url = `${searchUrl}${API_KEY}${urlPage}${urlQuery}`;
+//     } else {
+//       url = `${mainUrl}${API_KEY}${urlPage}`;
+//     }
+
+//     try {
+//       const res = await fetch(url);
+//       const data = await res.json();
+//       console.log(data);
+//       setPhotos((oldPhotos) => {
+//         console.log(oldPhotos);
+//         if (query && showPage === 1) {
+//           return data.results;
+//         } else if (query) {
+//           return [...oldPhotos, ...data.results];
+//         } else {
+//           return [...oldPhotos, ...data];
+//         }
+//       });
+
+//       setLoading(false);
+//     } catch (err) {
+//       console.log(err);
+//       setLoading(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchPhotos();
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [showPage]);
+
+//   useEffect(() => {
+//     const event = () => {
+//       if (
+//         window.scrollY + window.innerHeight >=
+//         document.body.scrollHeight - 10
+//       ) {
+//         setShowPage((oldShowPage) => oldShowPage + 1);
+//       }
+//     };
+//     window.addEventListener("scroll", event);
+//     return () => window.removeEventListener("scroll", event);
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, []);
+//   const submitSearch = (e) => {
+//     e.preventDefault();
+//     setShowPage(2);
+//   };
+
+//   return (
+//     <main>
+//       <section className="search">
+//         <form className="search-form">
+//           <input
+//             type="text"
+//             className="form-input"
+//             placeholder="search"
+//             value={query}
+//             onChange={(e) => setQuery(e.target.value)}
+//           />
+//           <button type="submit" className="submit-btn" onClick={submitSearch}>
+//             <FaSearch />
+//           </button>
+//         </form>
+//       </section>
+//       <section className="photos">
+//         <div className="photos-center">
+//           {photos.map((photo, index) => {
+//             // console.log(photo);
+//             return <Photo key={photo.id} {...photo} />;
+//           })}
+//         </div>
+//         {loading && <h2 className="loading">loading...</h2>}
+//       </section>
+//     </main>
+//   );
+// }
+
+// export default App;
+import React, { useState, useEffect, useRef } from "react";
+import { FaSearch } from "react-icons/fa";
+
+import Photo from "./Photo";
+const clientID = `?client_id=${process.env.REACT_APP_API_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [showPage, setShowPage] = useState(1);
+  const [page, setPage] = useState(1);
   const [query, setQuery] = useState("");
+  const [newImages, setNewImages] = useState(false);
+  const mounted = useRef(false);
 
-  const fetchPhotos = async () => {
+  const fetchImages = async () => {
     setLoading(true);
     let url;
-
-    const urlPage = `&page=${showPage}`;
+    const urlPage = `&page=${page}`;
     const urlQuery = `&query=${query}`;
-
     if (query) {
-      url = `${searchUrl}${API_KEY}${urlPage}${urlQuery}`;
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
     } else {
-      url = `${mainUrl}${API_KEY}${urlPage}`;
+      url = `${mainUrl}${clientID}${urlPage}`;
     }
-
     try {
-      const res = await fetch(url);
-      const data = await res.json();
-      console.log(data);
+      const response = await fetch(url);
+      const data = await response.json();
       setPhotos((oldPhotos) => {
-        if (query && showPage === 1) {
+        if (query && page === 1) {
           return data.results;
         } else if (query) {
           return [...oldPhotos, ...data.results];
@@ -39,59 +139,72 @@ function App() {
         }
       });
 
+      setNewImages(false);
       setLoading(false);
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      setNewImages(false);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
+
+  useEffect(() => {
+    if (!mounted.current) {
+      mounted.current = true;
+      return;
+    }
+    if (!newImages) return;
+    if (loading) return;
+    setPage((oldPage) => {
+      return oldPage + 1;
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [newImages]);
+
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImages(true);
     }
   };
 
   useEffect(() => {
-    fetchPhotos();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [showPage]);
-
-  useEffect(() => {
-    const event = () => {
-      if (
-        window.scrollY + window.innerHeight >=
-        document.body.scrollHeight - 10
-      ) {
-        setShowPage((oldShowPage) => oldShowPage + 1);
-      }
-    };
     window.addEventListener("scroll", event);
     return () => window.removeEventListener("scroll", event);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  const submitSearch = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setShowPage(2);
+    if (!query) return;
+    if (page === 1) {
+      fetchImages();
+    }
+    setPage(1);
   };
-
   return (
     <main>
       <section className="search">
         <form className="search-form">
           <input
             type="text"
-            className="form-input"
             placeholder="search"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
+            className="form-input"
           />
-          <button type="submit" className="submit-btn" onClick={submitSearch}>
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
             <FaSearch />
           </button>
         </form>
       </section>
       <section className="photos">
         <div className="photos-center">
-          {photos.map((photo, index) => {
-            // console.log(photo);
-            return <Photo key={photo.id} {...photo} />;
+          {photos.map((image, index) => {
+            return <Photo key={index} {...image} />;
           })}
         </div>
-        {loading && <h2 className="loading">loading...</h2>}
+        {loading && <h2 className="loading">Loading...</h2>}
       </section>
     </main>
   );
